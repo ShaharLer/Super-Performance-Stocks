ae35e76e752d26533a9f83b42597c5c94687bfb2
Shahar Lerner <shaharman5@gmail.com>
2020-10-16 17:37:31 +0300

    add endpoints for technical/breakout stocks

--- stocks_tracker/urls.py
+++ stocks_tracker/urls.py
@@ -4,10 +4,12 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('stock', views.StockViewSet)
+router.register('stocks/technical', views.TechnicallyValidStocksViewSet)
+router.register('stocks/breakout', views.BreakoutStocksViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('stocks/count/', views.count_stocks),
     path('scrapper/', views.stocks_scrapper),
     path('rater/', views.stock_rater),
     path('technical/', views.technically_valid_stocks),
--- stocks_tracker/views.py
+++ stocks_tracker/views.py
@@ -1,16 +1,33 @@
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import StockSerializer
-from stocks_tracker.utils.scrapper.marketwatch_scrapper import *
-from stocks_tracker.utils.rater.stocks_rater import *
-from stocks_tracker.utils.technical.technical_analsys_of_stock import *
+
 from stocks_tracker.utils.breakout.breakout_stocks import *
+from stocks_tracker.utils.rater.stocks_rater import *
+from stocks_tracker.utils.scrapper.marketwatch_scrapper import *
+from stocks_tracker.utils.technical.technical_analsys_of_stock import *
+from .serializers import TechnicalStockSerializer, BreakoutStockSerializer
 
 
-class StockViewSet(viewsets.ModelViewSet):
-    serializer_class = StockSerializer
-    queryset = Stock.objects.all().order_by('symbol')
+# class AllStockViewSet(viewsets.ModelViewSet):
+#     serializer_class = StockSerializer
+#     queryset = Stock.objects.filter(is_technically_valid=True).order_by('symbol')
+
+
+class TechnicallyValidStocksViewSet(viewsets.ModelViewSet):
+    serializer_class = TechnicalStockSerializer
+    queryset = Stock.objects.filter(is_technically_valid=True).order_by('symbol')
+
+
+class BreakoutStocksViewSet(viewsets.ModelViewSet):
+    serializer_class = BreakoutStockSerializer
+    queryset = Stock.objects.filter(last_breakout__isnull=False).order_by('symbol')
+
+
+@api_view(['GET'])
+def count_stocks(request):
+    total_stocks = Stock.objects.all().count()
+    return Response({"Total number of stocks in DB": total_stocks})
 
 
 @api_view(['GET'])
