f3de151b9a55b3a6739d44069f5934fecb5f25e7
Arad Inbar <arad.inbar@ibm.com>
2020-12-15 19:37:38 +0200

    adding 15-12 changes:

--- stocks_tracker/utils/volume/volume_watchlist_of_valid_stock.py
+++ stocks_tracker/utils/volume/volume_watchlist_of_valid_stock.py
@@ -10,16 +10,22 @@ import time as timer
 import time
 from yahoofinancials import YahooFinancials
 
+lock = threading.Lock()
 temp = 1
 stocks_dict = {}
 daily_avg_change = 0
 
 def compare_stock_relative_volume_to_avg(stock):
 
-    return 1
+
     stock_current_volume = stock.get_current_volume()
+
+
+    if (get_relative_market_time_today() == 0):
+        return str(round((stock.get_current_volume() / stock.get_three_month_avg_daily_volume())*100,2))+'%'
+
     stock_3m_avg_relative_volume = stock.get_three_month_avg_daily_volume() * get_relative_market_time_today()
-    return stock_current_volume / stock_3m_avg_relative_volume
+    return str(round((stock_current_volume / stock_3m_avg_relative_volume)*100,2)) + '%'
 
 def update_volume_watchlist_stock_dict(stock):
     global stocks_dict
@@ -28,25 +34,26 @@ def update_volume_watchlist_stock_dict(stock):
 
     stock_yahoo_obj = YahooFinancials(stock.symbol)
     stock_volume_increase_ratio = compare_stock_relative_volume_to_avg(stock_yahoo_obj)
-    #stock_price_change = stock_yahoo_obj.get_current_percent_change()
-    #print(stock_price_change)
-    stock_price_change = 2
-    daily_avg_change = daily_avg_change + stock_price_change
-    stocks_dict[stock.symbol] = [stock.eps_growth,stock.net_income_growth,stock.sales_growth,stock_volume_increase_ratio,stock_price_change]
+    stock_price_change = stock_yahoo_obj.get_current_percent_change()
+    stock_price_change = str(round((stock_price_change * 100 ),2))+'%'
+    current_stock_price = round(stock_yahoo_obj.get_current_price(),2)
+    stocks_dict[stock.symbol] = [stock.eps_growth,stock.net_income_growth,stock.sales_growth,stock_volume_increase_ratio,stock_price_change,current_stock_price]
 
 
 def get_specific_stock(stock_symbol):
     stock = Stock.objects.get(symbol=stock_symbol)
     if (stock != None):
+        lock.acquire()
         stock.is_stock_in_watchlist = True
         stock.save()
+        lock.release()
     update_volume_watchlist_stock_dict(stock)
     print(stocks_dict)
     return json.loads(json.dumps(stocks_dict))
 
 def volume_watchlist_of_valid_stock():
     global stocks_dict
-
+    stocks_dict = {}
     candidate_stocks = Stock.objects.filter(Q(is_stock_in_watchlist=True))
     start = time.time()
     if (len(candidate_stocks)!=0):
@@ -61,9 +68,11 @@ def volume_watchlist_of_valid_stock():
 def remove_stock_from_db(stock_symbol):
     stock = Stock.objects.get(symbol=stock_symbol)
     if (stock != None):
+        lock.acquire()
         stock.is_stock_in_watchlist = False
+        print(stock.is_stock_in_watchlist)
         stock.save()
-
+        lock.release()
 
 def volume_watchlist_of_valid_stock_main():
    return volume_watchlist_of_valid_stock()
